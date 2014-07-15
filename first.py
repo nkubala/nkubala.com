540b72970d209a1835a21f3f52c8f34fbbad3a6f
Nick <kubala.nick@gmail.com>
2014-07-15 01:26:37 +0000

    werkin

--- first.py
+++ first.py
@@ -2,26 +2,52 @@ from flask import Flask, render_template, url_for, redirect, make_response, sess
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def top_level():
 	return 'youre at the fuckin top'
 
+
+# @app.route('/index/')
+# def index(name=None):
+# #	return 'index'
+# 	if 'username' in session:
+# 		return 'Logged in as %s' % escape(session['username'])
+# 	return render_template('index.html', name=name)
+
 @app.route('/index/')
-def index(name=None):
-#	return 'index'
-	if 'username' in session:
-		return 'Logged in as %s' % escape(session['username'])
-	return render_template('index.html', name=name)
+def index():
+	user = {'nickname': 'nockj'} #fake user, placeholder
+	return render_template("index.html", 
+		title = 'Home',
+		user = user)
+
 
 @app.route('/testlate/')
-def testlate(name=None):
-	return render_template('first_template.html', name=name)
+def testlate():
+	user = {'nickname': 'nockj'} #fake user, placeholder
+	posts = [ # fake array of posts
+        { 
+            'author': { 'nickname': 'canno' , 'age': '8' }, 
+            'body': 'hhhhhhhhhhhhhhhhhhhhhhhhhhhhh' 
+        },
+        { 
+            'author': { 'nickname': 'chainsaw', 'age': '69' }, 
+            'body': 'zazazaza' 
+        }
+    ]
+	return render_template('first_template.html', 
+		title = 'test', 
+		user = user, 
+		posts = posts)
+
 
 #redirect test
 @app.route('/test/')
 def test():
 	# return redirect(url_for('index'))
 	return render_template('test.html')
+
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -33,6 +59,7 @@ def page_not_found(error):
 # Note the 404 after the render_template() call.
 # This tells Flask that the status code of that page should be 404 which means not found. 
 # By default 200 is assumed which translates to: all went well.
+
 
 
 
